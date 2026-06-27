377d09ef518de8838072c90f94682ffc4c8359dc
Snoopy-too <f.montoya@gmail.com>
2026-06-27 23:51:10 +0900

    Configure global timezone and database session to JST

--- src/Infrastructure/Database/Connection.php
+++ src/Infrastructure/Database/Connection.php
@@ -23,6 +23,7 @@ class Connection
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
         ]);
+        $this->pdo->exec("SET time_zone = '+09:00'");
     }
 
     public function getPDO(): PDO
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 declare(strict_types=1);
 
+// Set default timezone to Japan Standard Time (JST)
+date_default_timezone_set('Asia/Tokyo');
+
 require_once __DIR__ . '/autoloader.php';
 
 $config = require __DIR__ . '/../config.php';
